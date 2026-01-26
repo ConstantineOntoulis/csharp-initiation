fa3e5b610887e0e67465492ac49e57cf99e5200a
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-26 16:44:21 +0200

    Ch114: && and || if statements

--- 01-fundamentals/CSharpFoundation/Exercises/Ch14_LogicalOperators.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch14_LogicalOperators.cs
@@ -4,8 +4,23 @@ public static class Ch14_LogicalOperators
 {
     public static void Run()
     {
-        // Bro Code chapter 14: LogicalOperators
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 14: Logical Operators = Can be used to check if more than 1 condition is true/false
+
+        // &&
+        // ||
+
+        Console.WriteLine("What's the temperature outside: (C)");
+        double temp = Convert.ToDouble(Console.ReadLine());
+
+        if (temp >= 10 && temp <= 25)
+        {
+            Console.WriteLine("It's warm outside!");
+        }
+        else if (temp <= -50 || temp >= 50)
+        {
+            Console.WriteLine("DO NOT GO OUTSIDE!!!");
+        }
+
+        Console.ReadKey();
     }
 }
