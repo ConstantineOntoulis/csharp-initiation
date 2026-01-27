bc75391ac9970edb7a7136ba9069cc96fe0b599b
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-27 19:28:43 +0200

    Ch21: Arrays

--- 01-fundamentals/CSharpFoundation/Exercises/Ch21_Arrays.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch21_Arrays.cs
@@ -4,8 +4,23 @@ public static class Ch21_Arrays
 {
     public static void Run()
     {
-        // Bro Code chapter 21: Arrays
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 21: Arrays = a variable that can store multiple values. Has a fixed size
+
+        //String[] cars = { "BMW", "Mustang", Corvette" };
+
+        //Console.WriteLine(cars[0]);
+
+        String[] cars = new string[3]; //This creates an array with a fixed size. In this case, 3 items
+
+        cars[0] = "Tesla";
+        cars[1] = "Mustange";
+        cars[2] = "Corvette";
+
+        for (int i = 0; i < cars.Length; i++) //Displays all elements of the array
+        {
+            Console.WriteLine(cars[i]);
+        }
+
+        Console.ReadKey();
     }
 }
