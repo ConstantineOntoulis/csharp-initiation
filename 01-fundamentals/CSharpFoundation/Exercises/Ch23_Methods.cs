b0fe2bbfdaa847d71b1195af4fae8e9511964f70
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-28 17:50:52 +0200

    Ch23: Introduction to Methods

--- 01-fundamentals/CSharpFoundation/Exercises/Ch23_Methods.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch23_Methods.cs
@@ -4,8 +4,31 @@ public static class Ch23_Methods
 {
     public static void Run()
     {
-        // Bro Code chapter 23: Methods
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 23: Methods = performs a section of code, whenever it's called "invoked".
+        //                       benefit = Let's us reuse code w/o writing it multiple times
+        /*
+        Console.WriteLine("Happy birthday to you!");
+        Console.WriteLine("Happy birthday to you!");
+        Console.WriteLine("Happy birthday dear YOU!");
+        Console.WriteLine("Happy birthday to you!");
+        Console.WriteLine();
+        */
+        String name = "Constantine";
+        int age = 25;
+
+        singHappyBirthday(name, age);
+        singHappyBirthday(name, age);
+        singHappyBirthday(name, age);
+
+        Console.ReadKey();
+    }
+    static void singHappyBirthday(String name, int age)
+    {
+        Console.WriteLine("Happy birthday to you!");
+        Console.WriteLine("Happy birthday to you!");
+        Console.WriteLine("Happy birthday dear " + name + "!");
+        Console.WriteLine("You are now " + age + " years old!");
+        Console.WriteLine("Happy birthday to you!");
+        Console.WriteLine();
     }
 }
