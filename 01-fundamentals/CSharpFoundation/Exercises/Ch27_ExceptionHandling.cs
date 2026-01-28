fce807992f00f37290cf169c9881d7a0f8f508ab
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-28 18:15:06 +0200

    Ch27: Exception Handling

--- 01-fundamentals/CSharpFoundation/Exercises/Ch27_ExceptionHandling.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch27_ExceptionHandling.cs
@@ -4,8 +4,46 @@ public static class Ch27_ExceptionHandling
 {
     public static void Run()
     {
-        // Bro Code chapter 27: ExceptionHandling
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 27: ExceptionHandling
+        // errors that occur during execution
+
+        // try = try some code that is considered "dangerous"
+        // catch = catches and handles exceptions when they occur
+        // finally = always executes regardless if exception is caught or not
+
+        double x;
+        double y;
+        double result;
+        try
+        {
+            Console.WriteLine("Enter number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = x / y;
+
+            Console.WriteLine("Result: " + result);
+        }
+        catch(FormatException e)
+        {
+            Console.WriteLine("Enter ONLY numbers please!");
+        }
+        catch(DivideByZeroException e)
+        {
+            Console.WriteLine("You can't divide by zero!!");
+        }
+        /*
+        catch(Exception e) //this catches everything but it is not good practice because it doesn't let the user know what went wrong
+        {
+            Console.WriteLine("Something went wrong")
+        }
+        */
+        finally
+        {
+            Console.WriteLine("Thanks for visiting!");
+        }
+        Console.ReadKey();
     }
 }
