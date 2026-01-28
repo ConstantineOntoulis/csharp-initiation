e5b5091d68e27f8c02316d7f5dd29e69a7dd15d4
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-28 17:56:24 +0200

    Ch24: The "ReturnKeyword" in Methods

--- 01-fundamentals/CSharpFoundation/Exercises/Ch24_ReturnKeyword.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch24_ReturnKeyword.cs
@@ -4,8 +4,28 @@ public static class Ch24_ReturnKeyword
 {
     public static void Run()
     {
-        // Bro Code chapter 24: ReturnKeyword
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 24: ReturnKeyword
+        // Returns data back to the place where a method is invoked
+
+        double x;
+        double y;
+        double result;
+
+        Console.WriteLine("Enter in number 1: ");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter in number 2: ");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        result = Multiply(x, y);
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+    static double Multiply(double x, double y) //we change the origina "void" word into "double" because we are returning a double
+    {
+        double z = x * y;
+        return z; //or return x * y
     }
 }
