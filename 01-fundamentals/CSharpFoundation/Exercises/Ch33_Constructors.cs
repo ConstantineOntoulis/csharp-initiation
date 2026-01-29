6d9c0e403bcdae2702938e563400cc84c912b283
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-29 16:24:43 +0200

    Ch33: implement Car constructor and instantiate example cars

--- 01-fundamentals/CSharpFoundation/Exercises/Ch33_Constructors.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch33_Constructors.cs
@@ -4,8 +4,71 @@ public static class Ch33_Constructors
 {
     public static void Run()
     {
-        // Bro Code chapter 33: Constructors
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 33: Constructors
+        // A special method in a class
+        // Same name as the class name
+        // Can be used to assign argument to fields when creating an object
+        /*
+        Person person1 = new Person("Beth", 40);
+        Person person2 = new Person("Jerry", 43);
+
+        person1.Eat();
+        person1.Sleep();
+
+        person2.Eat();
+        person2.Sleep();
+        */
+
+        Car car1 = new Car("Ford", "Mustang", 2022, "red");
+        Car car2 = new Car("Chevy", "Corvette", 2021, "blue");
+
+        car1.Drive();
+        car2.Drive();
+
+        Console.ReadKey();
     }
+
+    class Car
+    {
+        String make;
+        String model;
+        int year;
+        String color;
+
+        public Car(String make, String model, int year, String color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("You drive the " + make + " " + model);
+        }
+    }
+    /*
+    class Person
+    {
+        public String name;
+        public int age;
+
+        public Person(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+    }*/
+
+
 }
