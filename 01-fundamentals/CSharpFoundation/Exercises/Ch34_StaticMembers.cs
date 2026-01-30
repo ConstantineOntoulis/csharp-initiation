e5bbcf983f751314c5b161803d3824f43305c53b
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-30 17:15:48 +0200

    Ch34: demonstrate static fields with shared Car count

--- 01-fundamentals/CSharpFoundation/Exercises/Ch34_StaticMembers.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch34_StaticMembers.cs
@@ -4,8 +4,41 @@ public static class Ch34_StaticMembers
 {
     public static void Run()
     {
-        // Bro Code chapter 34: StaticMembers
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 34: StaticMembers
+        // static = modifier to declare a static member, which belongs to the class itself
+        //          rather than to any specific object
+
+        Car car1 = new Car("Mustang");
+        Car car2 = new Car("Corvette");
+        Car car3 = new Car("Lambo");
+
+        /* Console.WriteLine(car1.numberOfCars); // if numberOfCars++ isn't static there is no real ownership for Car aka there would be 1 & 1 cars, not 2
+        Console.WriteLine(car2.numberOfCars); // numberOfCars++ needs to be static in order for the counter to
+        We can no longer access this field in a non-static way*/
+
+        // we can only access it now in a STATIC way
+
+        Console.WriteLine(Car.numberOfCars);
+
+        Car.StartRace();
+
+        Console.ReadKey();
+    }
+
+    class Car
+    {
+        String model;
+        public static int numberOfCars; // although bad practice, must be turned into a public so thaat Run() can access it
+
+        public Car(String model)
+        { 
+            this.model = model;
+            numberOfCars++; //this now belongs to the class and no one object has complete ownership of it
+        }
+
+        public static void StartRace() // static can be used for methods as well
+        {
+            Console.WriteLine("The race has begun!");
+        }
     }
 }
