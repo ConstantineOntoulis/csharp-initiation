18d5a5a07e96c5b50e47eeda091bf5bef6543e27
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-31 14:53:40 +0200

    Ch37: add abstract Vehicle base class demo

--- 01-fundamentals/CSharpFoundation/Exercises/Ch37_AbstractClasses.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch37_AbstractClasses.cs
@@ -1,11 +1,43 @@
-﻿namespace CSharpFoundation.Exercises;
+﻿using System.Reflection;
+
+namespace CSharpFoundation.Exercises;
 
 public static class Ch37_AbstractClasses
 {
     public static void Run()
     {
-        // Bro Code chapter 37: AbstractClasses
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 37: AbstractClasses
+        // Modifier that indicates missing components or incomplete implementation
+
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Boat bat = new Boat();
+        Vehicle vehicle = new Vehicle();
+
+        Console.ReadKey();
+    }
+    abstract class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+        int maxSpeed = 500;
+    }
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 50;
+    }
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+        int maxSpeed = 100;
     }
 }
