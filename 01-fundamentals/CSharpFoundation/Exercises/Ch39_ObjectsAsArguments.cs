093aec6e38ecc70b14904598ad8742a9ab35cb3f
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-01-31 17:26:52 +0200

    Ch39: add object copy helper (Copy) and demo independent instances

--- 01-fundamentals/CSharpFoundation/Exercises/Ch39_ObjectsAsArguments.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch39_ObjectsAsArguments.cs
@@ -4,8 +4,39 @@ public static class Ch39_ObjectsAsArguments
 {
     public static void Run()
     {
-        // Bro Code chapter 39: ObjectsAsArguments
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 39: ObjectsAsArguments
+
+        Car car1 = new Car("Mustang", "violet");
+
+        Car car2 = Copy(car1);
+
+        // ChangeColor(car1, "silver");
+
+        Console.WriteLine(car1.color + " " + car1.model);
+        Console.WriteLine(car2.color + " " + car2.model);
+
+        Console.ReadKey();
+    }
+    /*
+    public static void ChangeColor(Car car, String color)
+    {
+        car.color = color;
+    }
+    */
+
+    public static Car Copy(Car car)
+    {
+        return new Car(car.model, car.color);
+    }
+}
+public class Car
+{
+    public String model;
+    public String color;
+
+    public Car(String model, String color)
+    {
+        this.model = model;
+        this.color = color;
     }
 }
