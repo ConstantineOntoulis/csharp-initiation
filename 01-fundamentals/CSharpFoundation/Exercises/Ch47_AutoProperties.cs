deb1332d33347f439ffc80772d961414c20ae1b0
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-03 18:14:37 +0200

    Ch47: demonstrate auto-properties with Model { get; set; }

--- 01-fundamentals/CSharpFoundation/Exercises/Ch47_AutoProperties.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch47_AutoProperties.cs
@@ -1,11 +1,40 @@
-﻿namespace CSharpFoundation.Exercises;
+﻿using System.Xml.Schema;
+
+namespace CSharpFoundation.Exercises;
 
 public static class Ch47_AutoProperties
 {
     public static void Run()
     {
-        // Bro Code chapter 47: AutoProperties
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 47: AutoProperties
+        // shortcut when no additional logic is required in the proeprty
+        // do not have to define a field for a property,
+        // only have to write get; and/or set; inside the property
+
+        Car car = new Car("Porsche");
+
+        Console.WriteLine(car.Model);
+
+        Console.ReadKey();
+    }
+    class Car
+    {
+        /*
+        String model;
+
+        public String Model 
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        */
+        // OR
+        public String Model { get; set; }
+
+
+        public Car(String model) // constructor
+        {
+            this.Model = model;
+        }
     }
 }
