0f50b15c847baa0f2174809881ac3ed25863988a
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-03 17:53:53 +0200

    Ch46: add encapsulated Speed property with validation (max 500)

--- 01-fundamentals/CSharpFoundation/Exercises/Ch46_GettersAndSetters.cs
+++ 01-fundamentals/CSharpFoundation/Exercises/Ch46_GettersAndSetters.cs
@@ -1,11 +1,50 @@
-﻿namespace CSharpFoundation.Exercises;
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace CSharpFoundation.Exercises;
 
 public static class Ch46_GettersAndSetters
 {
     public static void Run()
     {
-        // Bro Code chapter 46: GettersAndSetters
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 46: GettersAndSetters
+        // add security to fields by encapsulation. They're accessors found within properties
+
+        // properties = combine aspects of both fields and methods (share name with a field)
+        // get accessor = used to return the property value
+        // set accessor = used to assign a new value
+        // value keyword = defines the value being assigned by the set (parameter)
+
+        Car car = new Car(400); //because public Car(int speed), therefore new Car(400 -> speed field)
+        car.Speed = 1000000000; //accessing the property instead of the field
+
+        Console.WriteLine(car.Speed); //accessing the property instead of the field if speed is written with a capital S
+
+        Console.ReadKey();
+    }
+
+    class Car
+    {
+        private int speed; //field but if we turned it to private, we would lose access
+        public Car(int speed)
+        {
+            Speed = speed;
+        }
+
+        public int Speed //property
+        {
+            get { return speed; }  //read whatever values are within the field
+            set // for this to be writable, we need set // writable
+            {
+                if (value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+                //speed = value; // value is like a parameter. when assigning a value to speed, that value is =value;
+            }
+        }
     }
 }
