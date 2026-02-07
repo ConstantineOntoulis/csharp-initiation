a377992026802df57bcfb20eb2d12978dba98278
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-07 18:30:20 +0200

    Add LINQ Ch52 WhereOperator filtering demo (books dataset)

--- 01-fundamentals/CSharpFoundation/LINQ/Ch52_WhereOperator.cs
+++ 01-fundamentals/CSharpFoundation/LINQ/Ch52_WhereOperator.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharpFoundation.LINQ;
 public static class Ch52_WhereOperator
 {
     public static void Run()
     {
+        //Where = Used for filtering data from a set of data.
+        // var cheapBooks = books.Where(b => b.Price < 20); // Method Based Syntax
+        // OR
+        // var cheapBooks = from b in books where b.Price < 20 select b; // Query Based Synstax
 
+        var books = Ch51_BookDatabase.GetBooksData(); // Get all the book data:
+        
+        var cheapBooks = books.Where(b => b.Price < 20);
+        //var cheapBooks = from b in books where b.Price < 20 select b;
+
+        foreach (var item in cheapBooks)
+        {
+            Console.WriteLine($"{item.Name} - {item.Price}");
+        }
+        
+        // if we want only the titles of the books that are under 20, then all we need to do is this:
+        /*
+        var cheapBooks = books.Where(b => b.Price < 20).Select(b => b.Name);
+
+        foreach (var item in cheapBooks)
+        {
+            Console.WriteLine($"{item}");
+        }
+        */
+        Console.ReadKey();
     }
 }
