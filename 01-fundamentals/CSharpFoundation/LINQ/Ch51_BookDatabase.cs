a595062a066b9964a229c24f042c59700f327c98
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-07 16:58:55 +0200

    Implement Ch51 Select operator examples (projection + tuples) using BookDatabase

---
+++ 01-fundamentals/CSharpFoundation/LINQ/Ch51_BookDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFoundation.LINQ
+{
+    public class Ch51_BookDatabase
+    {
+        public static IEnumerable<Ch51_Book> GetBooksData()
+        {
+            return new List<Ch51_Book>
+            {
+                new Ch51_Book(121, "Harry Potter and the Sorcerer's Stone", 20, "J.K. Rowling"),
+                new Ch51_Book(186, "Tell Me Your Dreams", 15, "Sidney Sheldon"),
+                new Ch51_Book(112, "The Monk Who Sold His Ferrari", 10, "Robin Sharma"),
+                new Ch51_Book(145, "Half Girlfriend", 10, "Chetan Bhagat"),
+                new Ch51_Book(167, "Harry Potter and the Half-Blood Prince", 20, "J.K. Rowling"),
+                new Ch51_Book(133, "Who Will Cry When You Die?", 10, "Robin Sharma"),
+                new Ch51_Book(177, "The 7 Habits of Highly Effective People", 25, "Stephen Covey")
+            };
+        }
+    }
+}
--- 01-fundamentals/CSharpFoundation/LINQ/Ch51_SelectOperator.cs
+++ 01-fundamentals/CSharpFoundation/LINQ/Ch51_SelectOperator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharpFoundation.LINQ;
 
@@ -8,6 +9,94 @@ public static class Ch51_SelectOperator
 {
     public static void Run()
     {
+        // Chapter 51: NestedLoops
+        // Language Integrated Query
+        // 2 synstaxes: Method Based and Query Based
+        // LINQ applications: LINQ to Objects (collections, files, Strings, etc.)
+        //                    LINQ to Entities (Entity Framework conceptual model)
+        //                    LINQ to XML (XML Documents)
+        //                    LINQ to DataSets (ADO.Net Data Sets)
 
+        //-----------------------------------------------------------------------------
+
+        // LINQ Extension Method based Syntax
+        // var output = books.Where(condition)
+        //                   .OrderBy(property)
+        //                   .Select(property);
+
+        // LINQ Query Operator based Syntax
+        // var output = from b in books
+        //                     where condition
+        //                     orderby property
+        //                     select property;
+
+        // SELECT Operator
+
+        // Used for selecting a particular propert out of an object
+        // SELECT Operator also allows to define a new type with selected property
+
+        // var authors = books.Select(b => b.Author);
+        
+        //OR
+
+        // var authors = from b in books select b.Author;
+
+        // Defining a new type using Select
+        /*
+        var data = books.Select(b => new
+        {
+            BookName = b.Name,
+            Cost = b.Prices
+        });
+        */
+
+        var books = Ch51_BookDatabase.GetBooksData();
+
+        var data = from b in books select b.Author;
+
+        // OR
+
+        // var data = books.Select(b => b.Author);
+        /*
+        var data1 = books.Select(b => new
+        {
+            BookName = b.Name,
+            Cost = b.Price
+        });
+        */
+
+        /*
+        var data1 = from b in books select new
+        {
+            BookName = b.Name,
+            Price = b.Price
+        };
+        */
+
+        // OR If you don't want to use "new", you can use Tuples
+
+        // var data1 = books.Select(b => (BookName: b.Name, BookPrice: b.Price));
+        var data1 = books.Select(b => (BookName: b.Name, BookPrice: b.Price)).ToList();
+
+        foreach (var book in data) {
+            Console.WriteLine(book);
+        }
+
+        Console.WriteLine();
+        // If you don't wanna use a foreach, there is another way (a shortcut) but that way ONLY works with lists.
+        // In this case, we will have to convert our data1 from enumerable to list.
+
+        /*
+        foreach (var item in data1)
+        {
+            Console.WriteLine($"{item.BookPrice} Euros - {item.BookName}");
+        }
+        */
+
+        data1.ForEach(b=> Console.WriteLine($"{b.BookPrice} Euros - {b.BookName}"));
+
+        Console.WriteLine();
+
+        Console.ReadKey();
     }
 }
