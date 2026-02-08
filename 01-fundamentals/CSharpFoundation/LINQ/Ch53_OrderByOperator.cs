d7aa4d60269a0c7f06c8a68100bb1bbee72cb8fe
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-08 20:43:26 +0200

    Ch53_OrderByOperator_Initiation

--- 01-fundamentals/CSharpFoundation/LINQ/Ch53_OrderByOperator.cs
+++ 01-fundamentals/CSharpFoundation/LINQ/Ch53_OrderByOperator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharpFoundation.LINQ;
 
@@ -8,6 +9,23 @@ public static class Ch53_OrderByOperator
 {
     public static void Run()
     {
+        // OrderBy: Used for ordering the data based on some property
+        // var booksCostWise = books.OrderBy(b => b.Price);
+        // var booksCostWise = books.OrderByDescending(b => b.Price);
 
+        // OR
+
+        // var booksCostWise = from b in books orderby b.Price select b;
+        // var booksCostWise = from b in books orderby b.Price descending select b;
+
+        var books = Ch51_BookDatabase.GetBooksData();
+
+        var booksCostWise = books.OrderBy(b => b.Price);
+        // var booksCostWise = books.OrderByDescending(b => b.Price);
+
+        foreach(var item in booksCostWise)
+        {
+            Console.WriteLine($"{item.Name} - {item.Price}");
+        }
     }
 }
