52bef0f531e198a7473365df217463d8a7577cfe
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-09 18:38:12 +0200

    Implement GroupBy projections and ordering by count

--- 01-fundamentals/CSharpFoundation/LINQ/Ch55_GroupByOperator.cs
+++ 01-fundamentals/CSharpFoundation/LINQ/Ch55_GroupByOperator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharpFoundation.LINQ;
 
@@ -8,6 +9,94 @@ public static class Ch55_GroupByOperator
 {
     public static void Run()
     {
+        // GroupBy Operator
+        // Used for grouping data based on any property
 
+        // var groupedBooks = books.GroupBy(b => b.Author);
+
+        // OR
+
+        // var groupedBooks = from b in books group b by b.Author
+
+        var books = Ch51_BookDatabase.GetBooksData();
+
+        var groupedBooks = books.GroupBy(b => b.Author);
+        /*
+        foreach(var item in groupedBooks) // Set of books associated with Author Key. This printing method makes it 
+        {                                 // Difficult to preview the data 
+            Console.WriteLine($"{item.Key}");
+        }
+        */
+        // Grouped data can be put into new list with the Grouped Key and other data
+        // Grouping and putting into new list
+
+        // var output = books.GroupBy(b => b.Author).Select(g => new
+        //              {
+        //                  Author = g.Key,
+        //                  BookCount = g.Count() // Count() is an extension LINQ method
+        //              });
+
+        // OR
+
+        // var output = from b in books
+        //                          group b by b.Author into g
+        //                          select new 
+        //                          {
+        //                              Author = g.Key,
+        //                              BookCount = g.Count()
+        //                          };
+        /*
+        var output = books.GroupBy(b => b.Author).Select(g => new
+        {
+            Author = g.Key,
+            BookCount = g.Count()
+        }).OrderByDescending(b => b.BookCount);
+        */
+
+        var output = books.GroupBy(b => b.Author).Select(g => new
+        {
+            Author = g.Key,
+            Books = g.Select(book => book)
+        });
+
+        // var output = from b in books group b by b.Author into g select new
+        // {
+        //      Author = g.Key,
+        //      BookCount = g.Count()
+        // };
+        /*
+        foreach (var item in output)
+        {
+            Console.WriteLine($"{item.Author} - {item.BookCount}");
+        }
+        */
+        /*
+        foreach (var item in output)
+        {
+            Console.WriteLine($"{item.Author}");
+            foreach(var book in item.Books)
+            {
+                Console.WriteLine($"    {book.Name} - {book.Price}");
+            }
+        }
+        */
+        // -----------------------------------------------------------------------------
+
+        var data = books.Where(b => b.Price < 15).GroupBy(b => b.Author).Select(g => new
+        {
+            Author = g.Key,
+            Books = g.Select(book => book.Name)
+        }).OrderBy(t => t.Author);
+
+        foreach (var item in data)
+        {
+            Console.WriteLine($"{item.Author}");
+            foreach (var book in item.Books)
+            {
+                Console.WriteLine($"    {book}");
+            }
+        }
+
+        Console.ReadKey();
     }
 }
