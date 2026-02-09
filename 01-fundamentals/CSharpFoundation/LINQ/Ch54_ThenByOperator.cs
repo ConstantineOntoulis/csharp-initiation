e758f8bc2165c9b0ed343aa79d22f9362ad8b9ac
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-09 17:07:03 +0200

    Add LINQ multi-key ordering example (OrderBy + ThenBy)

--- 01-fundamentals/CSharpFoundation/LINQ/Ch54_ThenByOperator.cs
+++ 01-fundamentals/CSharpFoundation/LINQ/Ch54_ThenByOperator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharpFoundation.LINQ;
 
@@ -8,6 +9,28 @@ public static class Ch54_ThenByOperator
 {
     public static void Run()
     {
+        // Multiple OrderBy
+        // After pplying one OrderBy, if there are same value records for that property,
+        // Another ordering can be done based on some other property.
 
+        // var booksCostWise = books.OrderBy(b => b.Price).ThenBy(b => b.Quantity);
+        
+        // OR
+
+        // var booksCostWise = from b in books orderby b.Price, b.Quantity select b;
+
+        var books = Ch51_BookDatabase.GetBooksData();
+
+        var data = books.OrderBy(b => b.Price).ThenBy(b => b.Name);
+        // var data = books.OrderByDescending(b => b.Price).ThenBy(b => b.Name);
+        // var data = from b in books orderby b.Price, b.Name select b;
+        // var data = from b in books orderby b.Price, b.Name descending select b;
+
+        foreach (var item in data)
+        {
+            Console.WriteLine($"{item.Name} - {item.Price} Euros"); 
+        }
+
+        Console.ReadKey();
     }
 }
