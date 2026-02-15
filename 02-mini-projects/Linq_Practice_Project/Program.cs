144a310b9d3b1cee935c6ffb6f5042da0de314f3
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-15 15:23:09 +0200

    TicketProgram Initiation

--- 02-mini-projects/Linq_Practice_Project/Program.cs
+++ 02-mini-projects/Linq_Practice_Project/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Linq_Practice_Project;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var ticketData = TicketDB.GetTicketData();
+
+        var openTickets = ticketData.Select(t => t.Status == "Open");
+
+        foreach(var ticket in openTickets)
+        {
+            Console.WriteLine($"{ticket}");
+        }
+
+        Console.ReadKey();
+    }
+}
