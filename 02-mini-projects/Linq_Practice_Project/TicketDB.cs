57f76eec914661ed8ecb537dcfb22b45bfb66d6b
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-15 14:25:14 +0200

    Linq Project Startup

---
+++ 02-mini-projects/Linq_Practice_Project/TicketDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Linq_Practice_Project
+{
+    internal class TicketDB
+    {
+        public static List<Ticket> Tickets new()
+        {
+
+        };
+    }
+}
