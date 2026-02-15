678a3bc93f1e73cab3a1f75df52002dbf5c9d6cc
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-15 14:55:46 +0200

    Ticket Class Creation

---
+++ 02-mini-projects/Linq_Practice_Project/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Linq_Practice_Project
+{
+    internal class Ticket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+
+        public Ticket(int id, string name, string status, DateTime date)
+        {
+            Id = id;
+            Name = name;
+            Status = status;
+            Date = date;
+        }
+    }
+
+}
