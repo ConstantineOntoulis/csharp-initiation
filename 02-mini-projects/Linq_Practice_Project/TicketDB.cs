40a05d5302666df087f0b9b52d369af8c66997a5
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-15 15:07:10 +0200

    TicketDB Creation

--- 02-mini-projects/Linq_Practice_Project/TicketDB.cs
+++ 02-mini-projects/Linq_Practice_Project/TicketDB.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
 using System.Text;
+using System.Linq;
 
 namespace Linq_Practice_Project
 {
     internal class TicketDB
     {
-        public static List<Ticket> Tickets new()
+        public static List<Ticket> GetTicketData()
         {
+            return new List<Ticket>()
+            {
+                new Ticket(1, "Login fails on invalid password", "Open", DateTime.Today.AddDays(-1)),
+                new Ticket(2, "Add pagination to tickets endpoint", "Open", DateTime.Today.AddDays(-3)),
+                new Ticket(3, "Fix N+1 query in comments", "Open", DateTime.Today.AddDays(-12)),
 
-        };
+                new Ticket(4, "Refactor DTO mapping", "InProgress", DateTime.Today.AddDays(-2)),
+                new Ticket(5, "Update Docker compose", "InProgress", DateTime.Today.AddDays(-9)),
+
+                new Ticket(6, "Improve validation messages", "Closed", DateTime.Today.AddDays(-5)),
+                new Ticket(7, "Add role-based authorization", "Closed", DateTime.Today.AddDays(-15)),
+
+                new Ticket(8, "Fix timezone bug", "Open", DateTime.Today.AddDays(-20)),
+            };
+        }
     }
 }
