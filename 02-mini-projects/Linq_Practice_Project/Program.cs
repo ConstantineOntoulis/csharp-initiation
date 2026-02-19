3f53393e8dc2d182168fe08badcd7b91a755dd9f
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-19 16:40:06 +0200

    Top 3 Latest & Summaries addition

--- 02-mini-projects/Linq_Practice_Project/Program.cs
+++ 02-mini-projects/Linq_Practice_Project/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 
 namespace Linq_Practice_Project;
@@ -13,6 +14,8 @@ public static class Program
         var ticketData = TicketDB.GetTicketData();
 
         var openTickets = ticketData.Where(t => t.Status == "Open").ToList();
+        var latestFirst = openTickets.OrderByDescending(d => d.Date);
+        var ticketSummary = ticketData.Select(s => (Id: s.Id, Title: s.Name, AgeDays: (DateTime.Today - s.Date).Days)).OrderByDescending(x => x.AgeDays).ToList();
 
         Console.WriteLine("The open tickets are:" );
         Console.WriteLine();
@@ -22,6 +25,21 @@ public static class Program
         foreach (var ticket in openTickets)
         {
             Console.WriteLine($"{ticket.Name}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("TOP 3 LATEST");
+        foreach(var ticket in latestFirst.Take(3))
+        {
+            Console.WriteLine($"#{ticket.Id} {ticket.Date} {ticket.Status} - {ticket.Name}");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("SUMMARIES (AGE DESC):");
+        foreach (var s in ticketSummary)
+        {
+            Console.WriteLine($"{s.Id} {s.AgeDays}d - {s.Title}");
         }
 
         Console.ReadKey();
