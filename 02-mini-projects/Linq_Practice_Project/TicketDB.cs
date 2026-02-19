8d12947fec2dbef9a4776b7ac94b2cb1b5bc5d8f
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-02-19 19:34:53 +0200

    Updating Ticket seeds

--- 02-mini-projects/Linq_Practice_Project/TicketDB.cs
+++ 02-mini-projects/Linq_Practice_Project/TicketDB.cs
@@ -11,17 +11,17 @@ namespace Linq_Practice_Project
         {
             return new List<Ticket>()
             {
-                new Ticket(1, "Login fails on invalid password", "Open", DateTime.Today.AddDays(-1)),
-                new Ticket(2, "Add pagination to tickets endpoint", "Open", DateTime.Today.AddDays(-3)),
-                new Ticket(3, "Fix N+1 query in comments", "Open", DateTime.Today.AddDays(-12)),
+                new Ticket(1, "Login fails on invalid password", TicketStatus.Open, DateTime.Today.AddDays(-1)),
+                new Ticket(2, "Add pagination to tickets endpoint", TicketStatus.Open, DateTime.Today.AddDays(-3)),
+                new Ticket(3, "Fix N+1 query in comments", TicketStatus.Open, DateTime.Today.AddDays(-12)),
 
-                new Ticket(4, "Refactor DTO mapping", "InProgress", DateTime.Today.AddDays(-2)),
-                new Ticket(5, "Update Docker compose", "InProgress", DateTime.Today.AddDays(-9)),
+                new Ticket(4, "Refactor DTO mapping", TicketStatus.InProgress, DateTime.Today.AddDays(-2)),
+                new Ticket(5, "Update Docker compose", TicketStatus.InProgress, DateTime.Today.AddDays(-9)),
 
-                new Ticket(6, "Improve validation messages", "Closed", DateTime.Today.AddDays(-5)),
-                new Ticket(7, "Add role-based authorization", "Closed", DateTime.Today.AddDays(-15)),
+                new Ticket(6, "Improve validation messages", TicketStatus.Closed, DateTime.Today.AddDays(-5)),
+                new Ticket(7, "Add role-based authorization", TicketStatus.Closed, DateTime.Today.AddDays(-15)),
 
-                new Ticket(8, "Fix timezone bug", "Open", DateTime.Today.AddDays(-20)),
+                new Ticket(8, "Fix timezone bug", TicketStatus.Open, DateTime.Today.AddDays(-20)),
             };
         }
     }
